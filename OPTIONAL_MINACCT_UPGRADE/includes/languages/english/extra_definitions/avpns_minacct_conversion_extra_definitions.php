35f0710507d75d70513459f06c4a4fae8f50c72a
lat9 <lat9@vinosdefrutastropicales.com>
2016-06-28 12:24:49 -0400

    Updates for Zen Cart 1.5.5+
    Update the plugin, removing files removed in Zen Cart 1.5.5 and later,
    compatibility scripts and the optional upgrade instructions for the
    minimum-customer-account plugin.

--- OPTIONAL_MINACCT_UPGRADE/includes/languages/english/extra_definitions/avpns_minacct_conversion_extra_definitions.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package languageDefines
- * @copyright Copyright 2015 Vinos de Frutas Tropicales (lat9) Minimum Customer Account Information -> Conversion to All Virtual Products -- No Shipping
- * @copyright Copyright 2003-2011 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
-*/
-define ('WARNING_ADDRESS_BOOK_CHECK_NEEDED', "We're sorry, but we've changed our account-information requirements and we need some more information about this address.  Please supply the required information.");
